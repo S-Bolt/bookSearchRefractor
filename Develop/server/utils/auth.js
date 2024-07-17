71fa6c6c09444cda8ae236a002b2b8569da7b495
Sam <sambolton.kw@gmail.com>
2024-07-17 17:49:34 -0400

    server/json dep, auth

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,14 +1,20 @@
 const jwt = require('jsonwebtoken');
+const { GraphQLError } = require('graphql');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +22,11 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      throw new GraphQLError('You have no token.', {
+        extensions: {
+          code: 'UNAUTHENTICATED',
+        },
+      });
     }
 
     // verify token and get user data out of it
@@ -25,11 +35,15 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      throw new GraphQLError('Invalid Token.', {
+        extensions: {
+          code: 'UNAUTHENTICATED',
+        },
+      });
     }
 
-    // send to next endpoint
-    next();
+    // return the request object
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
