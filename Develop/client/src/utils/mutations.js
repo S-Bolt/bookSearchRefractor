6590ae2d585b5eb5ba90da5623dbf4991b3be4a0
Sam <sambolton.kw@gmail.com>
2024-07-18 17:30:33 -0400

    starting mutations.js

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $ password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user{
+                _id
+                username
+                email
+            }
+        }
+    }
+
+`;
