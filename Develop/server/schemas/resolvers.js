89bf5d22ebd4ef1e29b0b5fe48b0950badbec614
Sam <sambolton.kw@gmail.com>
2024-07-17 23:54:57 -0400

    resolver partial taking break

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,2 +1,58 @@
+//import dependencies
 const { signToken, AuthenticationError } = require('../utils/auth');
 const { User } = require('./models');
+
+module.exports = {
+    Query:{
+        //get the user/saved books
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id}).populate('savedBooks');
+            }
+            throw AuthenticationError("User not authenticated.");
+        },
+    },
+
+    Mutation: {
+        //log the user in.
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne(( email));
+
+            if (!user) {
+                throw AuthenticationError("Can't find user.");
+            }
+
+            const correctPW = await user.isCorrectPassword(password);
+
+            if (!correctPW) {
+                throw AuthenticationError("Wrong password.");
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        //adding user
+        addUser: async (parent, args, context) => {
+            const user = await User.create(args);
+
+            if(!user){
+                throw AuthenticationError("Something went wrong");
+            }
+            
+            const token = signToken(user);
+            return { token, user};
+        },
+
+        saveBook: async (parent , args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOnAndUpdate(
+                    { _id: user._id},
+                    { $addToSet: {savedBooks: args.body}},
+                    { new: true, runValidators: true }
+                )
+            }
+        }
+    }
+    
+
+}
